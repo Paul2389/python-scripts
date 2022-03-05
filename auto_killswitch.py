b57276c1ae5d7e1767273276f637e26f1a01dfdf
Paul2389 <paul.tsekas@gmail.com>
2022-03-05 17:00:13 +0200

    Updated v2.0

--- auto_killswitch.py
+++ auto_killswitch.py
@@ -3,23 +3,44 @@
 
 import subprocess
 import time
+import re
 
 PASSWORD =''
 COMMAND='sudo ufw disable'
 
+CONNECTION_SUCCESS = False
+CONNECTION_PATTERN = 'Connection successfully activated'
 
 def killswitch_off():
-    subprocess.run('echo {} | sudo -S {}'.format(PASSWORD, COMMAND), shell=True)
+    subprocess.run('echo {} | sudo -S {}'.format(PASSWORD, COMMAND ), shell=True)
+    print('Killswitch is OFF')
 
 def vpn():
-    subprocess.run(['nmcli', 'con', 'up', 'id', 'Surfshark NL'])
-    time.sleep(5)
+    global CONNECTION_SUCCESS
+    vpn_connection = subprocess.run(['nmcli', 'con', 'up', 'id', 'Surfshark NL'], capture_output= True).stdout.decode()
+
+    if re.search(CONNECTION_PATTERN, vpn_connection):
+        CONNECTION_SUCCESS = True
+        return CONNECTION_SUCCESS
+    else:
+        CONNECTION_SUCCESS = False
+        return CONNECTION_SUCCESS
 
 def killswitch_on():
     subprocess.run(['sudo', 'ufw', 'enable'])
+    print('killswitch is ON')
 
 
+# Wait booting sequence
 time.sleep(10)
 killswitch_off()
-vpn()
+
+while CONNECTION_SUCCESS is False:
+    time.sleep(5)
+    print('Connecting to VPN...')
+    vpn()
+    if CONNECTION_SUCCESS is True:
+        print('Connection Successful!')
+        break
+
 killswitch_on()
